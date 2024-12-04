7709354efffd6eca881e21dbc8e82b5058efb6b7
Anna-tester14 <tester.valera@outlook.com>
2024-12-04 22:48:32 +0400

    dataProvider added

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,8 +1,5 @@
 package tests;
-
-
-import com.google.common.annotations.VisibleForTesting;
-import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -14,37 +11,23 @@ public class LoginTest extends BaseTest {
     public void correctLogin() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
+        assertTrue(productsPage.isDispl(), "");
         assertEquals(productsPage.getTitle(), "Products");
     }
 
-    @Test
-    public void lockedLogin() {
-        loginPage.open();
-        loginPage.login("locked_out_user", "secret_sauce");
-        assertEquals(driver.findElement(By.xpath("//h3")).getText(),
-                "Epic sadface: Sorry, this user has been locked out.");
+    @DataProvider()
+    public Object[][] loginData() {
+        return new Object[][]{
+                {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out"},
+                {"standard_users", "", "Epic sadface: Password is required"},
+                {"", "secret_sauce", "Epic sadface: Username is required"}
+        };
     }
 
-    @Test
-    public void errorLogin() {
+    @Test(dataProvider = "loginData")
+    public void lockedUserLogin(String user, String pass, String errorMsg) {
         loginPage.open();
-        loginPage.login("error_user", "secret_sauce");
-        assertEquals(productsPage.getTitle(), "Products");
-    }
-
-    @Test
-    public void incorrectLoginCheck() {
-        loginPage.open();
-        loginPage.login("standard_users", "secret_sauce");
-        assertTrue(driver.findElement(By.xpath("//*[text()='Products']")).isDisplayed());
-        assertEquals(driver.findElement(By.xpath("//*[@class='title']")).getText(), "Products");
-    }
-
-    @Test
-    public void emptyPasswordInputCheck() {
-        loginPage.open();
-        loginPage.login("standard_users", ".");
-        assertEquals(driver.findElement(By.xpath("//h3")).getText(),
-                "Epic sadface: Username and password do not match any user in this service");
+        loginPage.login(user, pass);
+        assertEquals(loginPage.getErrorMessage(), errorMsg);
     }
 }
